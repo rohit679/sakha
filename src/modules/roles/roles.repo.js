4e05930c6a4d8281bef2d7883ea46221af1c684a
Rohit Prasad <rohitprasad25061999@gmail.com>
2024-01-30 00:23:04 +0530

    role schema updated 🔨

--- src/modules/roles/roles.repo.js
+++ src/modules/roles/roles.repo.js
@@ -10,6 +10,14 @@ const roleSchema = new mongoose.Schema({
   },
   route_permission: {
     type: Array,
+  },
+  role_rank: {
+    type: String,
+    validate: {
+      validator: (v) => v > 0,
+      message: props => `${props.value} is not a role rank`
+    },
+    default: "100"
   }
 }, { strict: false, timestamps: true });
 
